3f89da1ad7ae62fe2e8b9ba2e8afb6dbac3a0d1d
heidless <rob.lockhart@yahoo.co.uk>
2023-09-21 12:12:07 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button>this is a button</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
